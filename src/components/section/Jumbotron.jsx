2723e396ab5be11fd9b134838d793f952d7e6cd4
WorkerHarder171 <dadybima171@gmail.com>
2024-10-26 21:48:16 +0700

    First Commit

--- src/components/section/Jumbotron.jsx
+++ src/components/section/Jumbotron.jsx
@@ -17,7 +17,7 @@ export default function Jumbotron() {
         className="absolute inset-0 my-auto mx-auto z-10"
       />
 
-      <div className="h-screen flex flex-col justify-center items-center relative z-0">
+      <div className="h-screen flex flex-col justify-center items-center relative z-10">
         <div className="text-center text-white">
           <p className="text-7xl capitalize leading-snug font-roboto mx-auto pt-12">
             Hey There! <br /> I'm Dady Bima Nur Sejati. <br /> A Frontend React
@@ -28,9 +28,8 @@ export default function Jumbotron() {
             and functional websites.
           </p>
         </div>
-
-        <div className="text-center mt-10">
-          <button className="capitalize text-white px-10 py-2.5 border border-white rounded-full duration-300 hover:bg-white hover:text-[#333]">
+        <div className="text-center mt-10 ">
+          <button className="capitalize z-20 text-white px-10 py-2.5 border border-white rounded-full duration-300 hover:bg-white hover:text-[#333]">
             Get in Touch
           </button>
           <SlArrowDown className="text-white text-3xl mt-10 mx-auto" />
