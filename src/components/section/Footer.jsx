0845b986802beb5374e433c30bdb1ee057a34651
WorkerHarder171 <dadybima171@gmail.com>
2024-10-27 11:38:51 +0700

    update portofolio

--- src/components/section/Footer.jsx
+++ src/components/section/Footer.jsx
@@ -1,5 +1,5 @@
 import { AiFillInstagram } from "react-icons/ai";
-import { FaLinkedin, FaFileDownload} from "react-icons/fa";
+import { FaLinkedin, FaFileDownload } from "react-icons/fa";
 import { FaSquareGithub } from "react-icons/fa6";
 import { MdEmail } from "react-icons/md";
 export default function Footer() {
@@ -12,33 +12,49 @@ export default function Footer() {
             Feel free to reach out to me for any queries or just to say hi!
           </p>
           <div className="wrapper-icon flex gap-5 items-center">
-            <a href="#" className="text-4xl">
+            <a href="https://www.instagram.com/dybim__/" className="text-4xl">
               <AiFillInstagram />
             </a>
-            <a href="#" className="text-4xl">
+            <a
+              href="https://www.linkedin.com/in/dady-bima/"
+              className="text-4xl"
+            >
               <FaLinkedin />
             </a>
-            <a href="#" className="text-4xl">
+            <a href="https://github.com/WorkerHarder171" className="text-4xl">
               <FaSquareGithub />
             </a>
             <i></i>
           </div>
         </div>
         <div className="wrapper-card flex justify-center gap-10 items-center ">
-        <button className="border border-[#2D302F] p-5 text-center rounded-[10px] duration-300 hover:bg-[#1E2020] w-[350px]">
-          <i className="text-4xl">
-            <MdEmail className="my-5 mx-auto" />
-          </i>
-          <p className="text-2xl">dadybima171@gmail.com</p>
-        </button>
-
-        <button className="border border-[#2D302F] p-5 text-center rounded-[10px] duration-300 hover:bg-[#1E2020] w-[350px]">
-          <i className="text-4xl">
-            <FaFileDownload className="my-5 mx-auto" />
-          </i>
-          <p className="text-2xl">My Resume</p>
-        </button>
-
+          <button
+          onClick={() =>
+              window.open(
+                "mailto:dadybima171@gmail.com",
+                "_blank"
+              )
+            }
+           className="border border-[#2D302F] p-5 text-center rounded-[10px] duration-300 hover:bg-[#1E2020] w-[350px]">
+            <i className="text-4xl">
+              <MdEmail className="my-5 mx-auto" />
+            </i>
+            <p className="text-2xl">dadybima171@gmail.com</p>
+          </button>
+          <button
+            onClick={() =>
+              window.open(
+                "https://drive.google.com/file/d/1e90Y3c5OexeRGw-CZgboX1UaiqGkzpsR/view?usp=sharing",
+                "_blank"
+              )
+            }
+            className="border border-[#2D302F] p-5 text-center rounded-[10px] duration-300 hover:bg-[#1E2020] w-[350px]"
+          >
+            <i className="text-4xl">
+              <FaFileDownload className="my-5 mx-auto" />
+            </i>
+            <p className="text-2xl">My Resume</p>
+          </button>
         </div>
       </footer>
     </>
