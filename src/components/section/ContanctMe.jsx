0871671141c5d665d43b8c89a19542e9ca0f7bad
WorkerHarder171 <dadybima171@gmail.com>
2024-10-27 15:02:03 +0700

    update portofolio

--- src/components/section/ContanctMe.jsx
+++ src/components/section/ContanctMe.jsx
@@ -12,13 +12,13 @@ export default function ContanctMe() {
             opportunities.
           </p>
         </div>
-        <form className="px-10 rounded-[10px] border border-[#2D302F] w-6/12">
+        <form className="px-10 rounded-[10px] border border-[#2D302F] bg-[#151817] w-6/12">
           <p className="text-3xl font-bold capitalize mt-10 mb-5">send us a message</p>
           <div className="wrapper flex flex-col gap-5">
-            <input type="text" placeholder="Fullname" className="input bg-transparent rounded-[10px] px-7 py-4 bg-[#2e2e2e]" />
-            <input type="email" placeholder="Email address" className="input bg-transparent rounded-[10px] px-7 py-4 bg-[#2e2e2e]" />
-            <input type="text" placeholder="Subject" className="input bg-transparent rounded-[10px] px-7 py-4 bg-[#2e2e2e]" />
-            <textarea placeholder="Your Message" className="input bg-transparent rounded-[10px] px-7 py-4 bg-[#2e2e2e]"></textarea>
+            <input type="text" placeholder="Fullname" className="input bg-transparent rounded-[10px] px-7 py-4 bg-[#1e1e1e]" />
+            <input type="email" placeholder="Email address" className="input bg-transparent rounded-[10px] px-7 py-4 bg-[#1e1e1e]" />
+            <input type="text" placeholder="Subject" className="input bg-transparent rounded-[10px] px-7 py-4 bg-[#1e1e1e]" />
+            <textarea placeholder="Your Message" className="input bg-transparent rounded-[10px] px-7 py-4 bg-[#1e1e1e]"></textarea>
             <button className="btn w-5/12 mb-10 p-5 rounded-[10px] duration-300 bg-[#6a0dad] font-semibold hover:bg-white hover:text-[#7c20e0]">Send Message</button>
           </div>
         </form>
